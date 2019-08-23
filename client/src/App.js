9bf8ed1395053e4c402178759b5e1b7b26f81561
Timothy McGowen <mcgowen17@yahoo.com>
2019-08-23 08:09:24 -0700

    First Commit Private Route set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import BubblePage from "./components/BubblePage"
+import PrivateRoute from './components/PrivateRoute';
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -12,8 +14,9 @@ function App() {
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
+          display BubblePage when you're authenticated (CHECK)
         */}
+        <PrivateRoute exact path="/bubblepage" component={BubblePage} />
       </div>
     </Router>
   );
